2e9a2e4c72ec7dc2eb0b355d7148807baf723e9d
privts <31954078+priv-ts@users.noreply.github.com>
2023-02-12 09:19:24 +0900

    Revert "Webdriver detail"

--- src/main/java/SampleCalc.java
+++
@@ -1,5 +0,0 @@
-public class SampleCalc {
-    public int add(int a, int b) {
-        return a * b;
-    }
-}
--- src/main/java/SampleScript.java
+++ src/main/java/SampleScript.java
@@ -6,17 +6,17 @@ import org.openqa.selenium.WebDriver;
 
 public class SampleScript {
     public static void main(String[] args) {
-        WebDriver firefoxDriver = new FirefoxDriver();
-        firefoxDriver.get("http://example.selenium.jp/reserveApp/");
-        firefoxDriver.findElement(By.id("guestname")).sendKeys("サンプルユーザ");
-        firefoxDriver.findElement(By.id("goto_next")).click();
-        firefoxDriver.quit();
+//        WebDriver driver = new FirefoxDriver();
+//        driver.get("http://example.selenium.jp/reserveApp/");
+//        driver.findElement(By.id("guestname")).sendKeys("サンプルユーザ");
+//        driver.findElement(By.id("goto_next")).click();
+//        driver.quit();
 
         WebDriverManager.chromedriver().setup();
-        WebDriver chromeDriver = new ChromeDriver();
-        chromeDriver.get("http://example.selenium.jp/reserveApp/");
-        chromeDriver.findElement(By.id("guestname")).sendKeys("サンプルユーザ");
-        chromeDriver.findElement(By.id("goto_next")).click();
-        chromeDriver.quit();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://example.selenium.jp/reserveApp/");
+        driver.findElement(By.id("guestname")).sendKeys("サンプルユーザ");
+        driver.findElement(By.id("goto_next")).click();
+        driver.quit();
     }
 }
